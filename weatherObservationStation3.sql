583368761093260b4c124e421ea9ffaa582f3ea9
bagusbpg <bagusbpg@gmail.com>
2021-12-26 22:33:58 +0700

    weatherObservationStation3 - hackerrank

---
+++ weatherObservationStation3.sql
@@ -0,0 +1,15 @@
+/*
+Query a list of CITY names from STATION for cities that have an even ID number.
+Print the result in any order, but exclude duplicates from the answer.
+The STATION table is described as follows:
+Field  Type
+ID     NUMBER
+CITY   VARCHAR2(21)
+STATE  VARCHAR2(2)
+LAT_N  NUMBER
+LONG-N NUMBER
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+
+SELECT DISTINCT CITY FROM STATION WHERE MOD(ID, 2) = 0;
