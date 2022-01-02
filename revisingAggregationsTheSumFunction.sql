748d5ceb6731a652367ed20c7979088d4ad397af
bagusbpg <bagusbpg@gmail.com>
2022-01-02 17:20:01 +0700

    revisingAggregationsTheSumFunction - hackerrank

---
+++ revisingAggregationsTheSumFunction.sql
@@ -0,0 +1,17 @@
+/*
+Query the total population of all cities in CITY where District is California. 
+
+Schema
+There is 1 table:
+CITY
+Field       Type
+ID          NUMBER
+NAME        VARCHAR2(17)
+COUNTRYCODE VARCHAR2(3)
+DISTRICT    VARCHAR2(20)
+POPULATION  NUMBER
+*/
+
+SELECT SUM(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California';
